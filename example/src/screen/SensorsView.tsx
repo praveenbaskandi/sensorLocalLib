98831594524e9d0cd91448b00df60d2b338eec7d
Praveen Baskandi <praveenbaskandi@Praveens-MacBook-Air.local>
2026-01-18 03:50:55 +0530

    fix: minor fixes and changes

--- example/src/screen/SensorsView.tsx
+++ example/src/screen/SensorsView.tsx
@@ -44,44 +44,32 @@ export function SensorsView() {
           title="Location Data:"
           description={location ? '' : 'No location data yet'}
         >
-          <RowBoxContent title="Heading" content={location?.heading} />
-          <RowBoxContent title="Speed" content={location?.speed} />
-          <RowBoxContent title="Altitude" content={location?.altitude} />
-          <RowBoxContent title="TimeStamp" content={location?.timestamp} />
-          <RowBoxContent title="Accuracy" content={location?.accuracy} />
-          <RowBoxContent title="Longitude" content={location?.longitude} />
-          <RowBoxContent title="Latitude" content={location?.latitude} />
-        </CardUI>
-
-        {/* <View style={styles.section}>
-          <Text style={styles.subHeader}>Location Data:</Text>
           {location ? (
-            <Text style={styles.jsonText}>
-              {JSON.stringify(location, null, 2)}
-            </Text>
-          ) : (
-            <Text>No location data yet</Text>
-          )}
-        </View> */}
-
+            <>
+              <RowBoxContent title="Heading" content={location?.heading} />
+              <RowBoxContent title="Speed" content={location?.speed} />
+              <RowBoxContent title="Altitude" content={location?.altitude} />
+              <RowBoxContent title="TimeStamp" content={location?.timestamp} />
+              <RowBoxContent title="Accuracy" content={location?.accuracy} />
+              <RowBoxContent title="Longitude" content={location?.longitude} />
+              <RowBoxContent title="Latitude" content={location?.latitude} />
+            </>
+          ) : null}
+        </CardUI>
         <CardUI
           mt={10}
           title="Gyroscope Data:"
           description={gyro ? '' : 'No gyroscope data yet'}
         >
-          <RowBoxContent title="Timestamp" content={gyro?.timestamp} />
-          <RowBoxContent title="Z" content={gyro?.z} />
-          <RowBoxContent title="Y" content={gyro?.y} />
-          <RowBoxContent title="X" content={gyro?.x} />
-        </CardUI>
-        {/* <View style={styles.section}>
-          <Text style={styles.subHeader}>Gyroscope Data:</Text>
           {gyro ? (
-            <Text style={styles.jsonText}>{JSON.stringify(gyro, null, 2)}</Text>
-          ) : (
-            <Text>No gyroscope data yet</Text>
-          )}
-        </View> */}
+            <>
+              <RowBoxContent title="Timestamp" content={gyro?.timestamp} />
+              <RowBoxContent title="Z" content={gyro?.z} />
+              <RowBoxContent title="Y" content={gyro?.y} />
+              <RowBoxContent title="X" content={gyro?.x} />
+            </>
+          ) : null}
+        </CardUI>
       </ScrollView>
     </SafeAreaView>
   );
